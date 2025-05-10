fa6f813e059c0d1d9a49630ce99cedc1d6b4bc9b
Johanna Lundström <johanna.ml.lundstrom@gmail.com>
2025-05-10 12:55:48 +0200

    Add consumer for receiving crud notifications and persisting statuses

--- src/main/java/task/service/consumers/ItemEventConsumer.java
+++ src/main/java/task/service/consumers/ItemEventConsumer.java
@@ -11,8 +11,7 @@ import task.service.repos.TaskStatusRepository;
 import task.service.services.TaskStatusService;
 
 @ApplicationScoped
-public class ItemEventConsumer
-{
+public class ItemEventConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ItemEventConsumer.class);
 
@@ -28,9 +27,7 @@ public class ItemEventConsumer
     @Incoming("created")
     public void consumeCreated(final String json)
     {
-
-        try
-        {
+        try {
             var event = objectMapper.readValue(json, ItemEvent.class);
             LOGGER.info("Received create event: {}", event.getEvent());
             LOGGER.info("Item create UID: {}", event.getItemUid());
@@ -38,13 +35,13 @@ public class ItemEventConsumer
             var taskStatus = taskStatusService.createStatus(event);
             repository.persist(taskStatus);
 
-            var priority = repository.findPriorityByUserUid(event.getUserUid()).size();
-            var completed = repository.findCompletedByUserUid(event.getUserUid()).size();
+            var priorityCount = repository.findPriorityByUserUid(event.getUserUid()).size();
+            var task = repository.findByUid(event.getItemUid());
 
-            taskStatusService.feedback(event.getType(), taskStatus, priority, completed);
+            taskStatusService.feedback(event.getEvent(), false, false, task.isCompleted(), priorityCount);
+            LOGGER.debug("Processed feedback");
 
-        } catch (final Exception e)
-        {
+        } catch (final Exception e) {
             LOGGER.error("Failed to process item-created event", e);
         }
     }
@@ -52,24 +49,26 @@ public class ItemEventConsumer
     @Incoming("updated")
     public void consumeUpdated(final String json)
     {
-        try
-        {
+        try {
             var event = objectMapper.readValue(json, ItemEvent.class);
             LOGGER.info("Received update event: {}", event.getEvent());
             LOGGER.info("Item update UID: {}", event.getItemUid());
 
             var prevStatus = repository.findByUid(event.getItemUid());
-            var taskStatus = taskStatusService.updateStatus(event, prevStatus);
 
-            repository.update(taskStatus);
+            var wasCompleted = prevStatus.isCompleted();
+            var wasPriority = prevStatus.isPriority();
 
-            var priority = repository.findPriorityByUserUid(event.getUserUid()).size();
-            var completed = repository.findCompletedByUserUid(event.getUserUid()).size();
+            var updatedStatus = taskStatusService.updateStatus(event, prevStatus);
+            repository.update(updatedStatus);
 
-            taskStatusService.feedback(event.getType(), prevStatus, priority, completed);
+            var priorityCount = repository.findPriorityByUserUid(event.getUserUid()).size();
 
-        } catch (final Exception e)
-        {
+            taskStatusService.feedback(event.getEvent(), wasCompleted, wasPriority, updatedStatus.isCompleted(),
+                    priorityCount);
+            LOGGER.debug("Processed feedback");
+
+        } catch (final Exception e) {
             LOGGER.error("Failed to process item-update event", e);
         }
     }
@@ -77,24 +76,27 @@ public class ItemEventConsumer
     @Incoming("deleted")
     public void consumeDeleted(final String json)
     {
-        try
-        {
+
+        try {
             var event = objectMapper.readValue(json, ItemEvent.class);
             LOGGER.info("Received delete event: {}", event.getEvent());
             LOGGER.info("Item delete UID: {}", event.getItemUid());
 
-            var prevStatus = repository.findByUid(event.getItemUid());
-            var taskStatus = taskStatusService.updateStatus(event, prevStatus);
+            var statusToBeDeleted = repository.findByUid(event.getItemUid());
 
-            repository.delete(taskStatus);
+            var wasCompleted = statusToBeDeleted.isCompleted();
+            var wasPriority = statusToBeDeleted.isPriority();
 
-            var priority = repository.findPriorityByUserUid(event.getUserUid()).size();
-            var completed = repository.findCompletedByUserUid(event.getUserUid()).size();
+            repository.delete(statusToBeDeleted);
 
-            taskStatusService.feedback(event.getType(), prevStatus, priority, completed);
+            var priorityCount = repository.findPriorityByUserUid(event.getUserUid()).size();
 
-        } catch (final Exception e)
-        {
+            taskStatusService.feedback(event.getEvent(), wasCompleted, wasPriority, statusToBeDeleted.isCompleted(),
+                    priorityCount);
+
+            LOGGER.debug("Processed feedback");
+
+        } catch (final Exception e) {
             LOGGER.error("Failed to process item-delete event", e);
         }
     }
--- src/main/java/task/service/services/TaskStatusService.java
+++ src/main/java/task/service/services/TaskStatusService.java
@@ -13,35 +13,107 @@ public class TaskStatusService
 
     public TaskStatus createStatus(final ItemEvent event)
     {
-        return new TaskStatus();
+        if (event.getType() == null)
+        {
+            return new TaskStatus();
+        }
+
+        var status = new TaskStatus();
+        status.setUid(event.getItemUid());
+        status.setUserUid(event.getUserUid());
+        status.setType(event.getType());
+        status.setPriority(event.getType().equals("priority"));
+        status.setCompleted(event.getType().equals("completed"));
+        status.setCreatedAt(event.getTime());
+        status.setUpdatedAt(event.getTime());
+
+        if (event.getType().equals("completed"))
+        {
+            status.setCompletedAt(event.getTime());
+        }
+
+        return status;
     }
 
     public TaskStatus updateStatus(final ItemEvent event, final TaskStatus prevStatus)
     {
-        return new TaskStatus();
+        var status = prevStatus;
+        var content = event.getContent();
+
+        status.setPriority(content.containsKey("priority") && (boolean) content.get("priority"));
+        status.setCompleted(content.containsKey("completed") && (boolean) content.get("completed"));
+        status.setUpdatedAt(content.containsKey("time") ? (long) content.get("time") : event.getTime());
+
+        if (event.getType().equals("completed"))
+        {
+            status.setCompletedAt(event.getTime());
+        }
+
+        return status;
     }
 
-    public void feedback(final String eventType, final TaskStatus taskStatus, final int priority, final int completed)
+    public void feedback(final String eventType, final boolean wasCompleted, final boolean wasPriority,
+            final boolean completed, final int priorityCount)
     {
-        if (!eventType.equals("item-delete"))
-        {
-            if (taskStatus.isCompleted())
-            {
-                LOGGER.info("Task is completed");
-            }
-        }
 
-        if (priority >= 10)
+        switch (eventType)
+        {
+            case "item-created" -> createFeedback(wasCompleted, wasPriority, completed, priorityCount);
+            case "item-updated" -> updateFeedback(wasCompleted, wasPriority, completed, priorityCount);
+            case "item-deleted" -> deleteFeedback(wasPriority, priorityCount);
+        }
+    }
+
+    private void createFeedback(final boolean wasCompleted, final boolean wasPriority, final boolean completed,
+            int priorityCount)
+    {
+
+        if (!wasCompleted && completed)
         {
             // todo: send feedback event
-            LOGGER.info("Sending feedback event for priority, priority count is{}", priority);
+            LOGGER.info("Sending feedback event for completed item");
         }
 
-        if (taskStatus.isCompleted() && completed == 5)
+        if (!wasPriority && priorityCount >= 6)
         {
             // todo: send feedback event
-            LOGGER.info("Sending feedback event for completed, completed count is{}", priority);
+            LOGGER.info("Sending feedback event for priority limit is reached");
+        } else
+        {
+            LOGGER.debug("Priority limit not reached");
+        }
+    }
 
+    private void updateFeedback(final boolean wasCompleted, final boolean wasPriority, final boolean completed,
+            int priorityCount)
+    {
+        if (!wasCompleted && completed)
+        {
+            // todo: send feedback event
+            LOGGER.info("Sending feedback event for newly completed item");
+        }
+
+        // todo: only send this feedback if it has changed from under to over etc.
+        if (wasPriority && priorityCount >= 6)
+        {
+            // todo: send feedback event
+            LOGGER.info("Sending feedback event for priority limit is reached");
+        } else
+        {
+            LOGGER.debug("Priority limit not reached");
+        }
+    }
+
+    private void deleteFeedback(final boolean wasPriority, final int priorityCount)
+    {
+        // todo: only send this feedback if it has changed from under to over etc.
+        if (wasPriority && priorityCount >= 6)
+        {
+            // todo: send feedback event
+            LOGGER.info("Sending feedback event for priority, priority count is {}", priorityCount);
+        } else
+        {
+            LOGGER.debug("Priority limit not reached");
         }
     }
 }
